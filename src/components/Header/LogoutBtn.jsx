32e11ccbbb0721f685f0bc161b7f1bdf81f9df93
KStar-coder <sharmakaustav04@gmail.com>
2024-05-28 09:28:55 +0530

    Commit

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import authService from '../../appwrite/auth'
+import { logout } from '../../store/authSlice'
+
 
 function LogoutBtn() {
+
+    const dispatch = useDispatch()
+    const logoutHandler = () => {
+        authService.logout().then(() => {
+            dispatch(logout())
+        })
+    }
+
     return (
-        <div>LogoutBtn</div>
+        <button
+            className='inline-block px-6 py-2 duration-200 bg-sky-500 hover:bg-sky-300 shadow-md rounded-full'
+        >Logout</button>
     )
 }
 
