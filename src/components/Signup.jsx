2782eddfc8775de55b22320b989574bab00905bc
KStar-coder <sharmakaustav04@gmail.com>
2024-05-31 12:53:47 +0530

    Commit

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -32,7 +32,56 @@ function Signup() {
     }
 
     return (
-        <div>Signup</div>
+        <div className='flex items-center justify-center'>
+            <div className={`mx-auto w-full max-w-lg bg-[#141E46] rounded-xl p-10 border-black`}>
+                <div className='mb-2 flex justify-center'>
+                    <span className='inline-block w-full max-w-[100px]'>
+                        <Logo width='100%' />
+                    </span>
+                </div>
+
+                <h2 className='text-center text-2xl font-bold leading-tight'>Sign up to your account</h2>
+                <p className='mt-2 text-center text-base text-sky-300'>
+
+                    Already have an account?&nbsp;
+                    <Link
+                        to='/login'
+                        className='font-medium text-sky-100 transition-all duration-200 hover:underline'
+                    >
+                        Sign In
+                    </Link>
+                </p>
+
+                {error && <p className='text-red-400 mt-8 text-center'>{error}</p>}
+
+                <form onSubmit={handleSubmit(create)}>
+                    <div className='space-y-5'>
+
+                        <Input
+                            label="Full Name : "
+                            placeholder="Enter your full name"
+                            {...register("name", {
+                                required: true,
+                            })}
+                        />
+                        <Input
+                            label='Email: '
+                            placeholder='Enter your email'
+                            type="email"
+                            {...register("email"), {
+                                required: true,
+                                validate: {
+                                    matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                                        "Email address must be a valid address",
+                                }
+                            }}
+                        />
+
+                    </div>
+                </form>
+
+            </div>
+        </div>
     )
 }
 
